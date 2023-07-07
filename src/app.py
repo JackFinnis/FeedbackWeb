276808f60bd6848c12219b3fe04d0d9667fa2f99
Jack Finnis <jack.finnis@icloud.com>
2023-07-07 14:56:49 +0100

    Improved subject and new subject pages

--- src/app.py
+++ src/app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, redirect
-from firebase_admin import credentials, firestore, initialize_app
+from firebase_admin import credentials, firestore, initialize_app, storage
 from models import *
 import random
 import string
 
 flask_app = Flask(__name__)
-cred = credentials.Certificate('key.json')
+cred = credentials.Certificate('creds.json')
 firebase_app = initialize_app(cred)
 db = firestore.client()
 subjects = db.collection('subject')
+
+def upload_photo(photo_url, public_id):
+	client = storage.storage.Client.from_service_account_json('key.json')
+	ref = client.get_bucket('logos').blob(public_id)
+	ref.upload_from_filename(photo_url)
+	return ref.public_url
 
 def fetch_subject(private_id):
 	docs = subjects.where('private_id', '==', private_id).stream()
@@ -23,13 +29,14 @@ def root():
 
 @flask_app.route('/subject/<private_id>')
 def subject(private_id):
+	subject=Subject(1, 2, 'Apple', 'Please let us know how satisfied you are with your product and how we could have improved our customer service today.', '/static/jack.jpeg')
+	return render_template('subject.html', feedbacks=[Feedback('Too expensive', '3', firestore.firestore.SERVER_TIMESTAMP)], subject=subject)
+
 	subject = fetch_subject(private_id)
-	title = subject.title
-	public_id = subject.public_id
-	docs = subjects.document(public_id).collection('feedback').get()
+	docs = subjects.document(subject.public_id).collection('feedback').get()
 	feedbacks = map(lambda x: Feedback.from_dict(x.to_dict()), docs)
 	
-	return render_template('subject.html', feedbacks=feedbacks, public_id=public_id, title=title, private_id=private_id)
+	return render_template('subject.html', feedbacks=feedbacks, subject=subject)
 
 @flask_app.route('/new')
 def new_subject():
@@ -37,38 +44,52 @@ def new_subject():
 
 @flask_app.route('/new/submit', methods = ['POST'])
 def submit_new_subject():
-	title = request.form['title']
-	description = request.form['description']
+	return redirect(f'/subject/{1}')
+
 	public_id = get_id()
 	private_id = get_id()
-	new_topic = Subject(private_id, public_id, title, description)
+
+	title = request.form['title']
+	description = request.form['description']
+	photo_url = request.form['logo']
+	public_url = upload_photo(photo_url, public_id)
+
+	new_topic = Subject(private_id, public_id, title, description, public_url)
 	subjects.document(public_id).set(new_topic.to_dict())
 	
 	return redirect(f'/subject/{private_id}')
 
 @flask_app.route('/send/<public_id>')
 def send_feedback(public_id):
+	subject=Subject(1, 2, 'Apple', 'Please let us know how satisfied you are with your product and how we could have improved our customer service today.', '/static/jack.jpeg')
+	return render_template('send_feedback.html', subject=subject)
+
 	subject = Subject.from_dict(subjects.document(public_id).get().to_dict())
-	publicID = subject.public_id
-	title = subject.title
-	description = subject.description
 	
 	return render_template('send_feedback.html', public_id=public_id, title=title, description=description)
 
 @flask_app.route('/send/submit/<public_id>', methods = ['POST'])
 def submit_feedback(public_id):
+	return render_template('submitted_feedback.html', public_id=2)
+
 	feedback = request.form['feedback']
 	feedback_id = get_id()
-	feedback = Feedback(feedback, feedback_id)
+	feedback = Feedback(feedback, feedback_id, firestore.firestore.SERVER_TIMESTAMP)
 	subjects.document(public_id).collection('feedback').document(feedback_id).set(feedback.to_dict())
     
 	return render_template('submitted_feedback.html', public_id=public_id)
 
 @flask_app.route('/delete_feedback/<feedback_id>/<private_id>')
 def delete_feedback(feedback_id, private_id):
+	return redirect(f"/subject/{1}")
+
 	subject = fetch_subject(private_id)
 	subjects.document(subject.public_id).collection('feedback').document(feedback_id).delete()
 
 	return redirect(f"/subject/{private_id}")
+
+@flask_app.errorhandler(404)
+def handle_404(e):
+	return render_template('404.html')
 
 flask_app.run()
--- src/models.py
+++ src/models.py
@@ -1,16 +1,18 @@
 class Subject:
-    def __init__(self, private_id, public_id, title, description):
+    def __init__(self, private_id, public_id, title, description, photo_url):
         self.private_id = private_id
         self.public_id = public_id
         self.title = title
         self.description = description
+        self.photo_url = photo_url
     
     def to_dict(self):
         return {
             'private_id': self.private_id,
             'public_id': self.public_id,
             'title': self.title,
-            'description': self.description
+            'description': self.description,
+            'photo_url': self.photo_url
         }
 
     @staticmethod
@@ -20,22 +22,26 @@ class Subject:
             dict['public_id'],
             dict['title'],
             dict['description'],
+            dict['photo_url']
         )
     
 class Feedback:
-    def __init__(self, feedback, feedback_id):
+    def __init__(self, feedback, feedback_id, timestamp):
         self.feedback = feedback
         self.feedback_id = feedback_id
+        self.timestamp = timestamp
     
     def to_dict(self):
         return {
             'feedback': self.feedback,
-            'feedback_id': self.feedback_id
+            'feedback_id': self.feedback_id,
+            'timestamp': self.timestamp
         }
 
     @staticmethod
     def from_dict(dict):
         return Feedback(
             dict['feedback'],
-            dict['feedback_id']
+            dict['feedback_id'],
+            dict['timestamp']
         )
